8971a48324c633f93ce75a0a5247bec78f12a791
Risheb.s <rishebs123456@gmail.com>
2026-07-10 17:18:00 +0530

    crm performance enhnacements

--- core/settings.py
+++ core/settings.py
@@ -103,7 +103,7 @@ REST_FRAMEWORK = {
         "rest_framework.authentication.SessionAuthentication",
         "rest_framework.authentication.BasicAuthentication",
     ],
-    "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.PageNumberPagination",
+    "DEFAULT_PAGINATION_CLASS": "core.pagination.CustomPageNumberPagination",
     "PAGE_SIZE": 100,
 }
 
